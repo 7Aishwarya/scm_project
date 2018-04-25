40c292e8a8e977ac44c054fa23816a9c1a0bd051
GARISHMA VIRK <500060134@DDN.UPES.AC.IN>
2018-04-25 21:57:11 +0530

    by garishma at 9.57

--- scmcode.cpp
+++ scmcode.cpp
@@ -115,6 +115,54 @@ private:
      void freeNode(Node* leaf);
 
 };
+int func5()
+{
+
+   int num;
+
+   bool flag = true;
+
+   cout<<"Enter any number(should be positive integer): ";
+
+   cin>>num;
+
+   for(int i = 2; i <= num / 2; i++) 
+{
+
+      if(num % i == 0) 
+{
+
+         flag = false;
+
+         break;
+
+}
+
+}
+
+   if (flag==true)
+{
+
+      cout<<num<<" is a prime number";
+
+      cout<<"\n";
+
+}
+
+   else
+{
+
+      cout<<num<<" is not a prime number";
+
+      cout<<"\n";
+
+}
+
+   return 0;
+
+}
+
+
 
 
 
